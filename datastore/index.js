cd0e1e49f88418f4d8ad379a2990a5a8ac6e923f
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-24 16:37:46 -0400

    completed creating new todos

--- datastore/index.js
+++ datastore/index.js
@@ -8,9 +8,12 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    //console.log('dataDir:', + exports.dataDir, ' path:' + '/' + id + '.txt');
+    fs.appendFile(exports.dataDir + '/' + id + '.txt',
+    text,
+    () => callback(null, { id, text }));
+  });
 };
 
 exports.readAll = (callback) => {
